3f362da457585072bf4eb3848433f9d6f4eba41f
satishmahat <mahatsatish@gmail.com>
2025-03-06 19:14:24 +0545

    changed nav logo layout

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./navbar.css"
 import { Link } from 'react-router-dom'
+import { FiMenu } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
 // import "../pages/Homepage"
 const Navbar = () => {
 
@@ -13,20 +15,26 @@ const Navbar = () => {
     <>
     <nav>
     <div className="wrapper">
-        <div className="logo"><Link to="/home">OSD2</Link></div>
+        {/* Updated Logo Structure */}
+        <div className="logo">
+            <Link to="/home">
+              <span className="logo-main">OSD2</span>
+              <span className="logo-sub">Osho Sustainable Designers & Developers Pvt. Ltd.</span>
+            </Link>
+          </div>
         <input type="radio" name="slider" id="menu-btn" />
         <input type="radio" name="slider" id="close-btn" />
         <ul className="nav-links">
-        <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times" />X</label>
+        <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times" /><IoClose /></label>
         <li><Link to="/home" onClick={handleCloseNav}>Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/career">Career</Link></li>
-        <li><Link to="/contacts">Contacts</Link></li>
+        <li><Link to="/about " onClick={handleCloseNav}>About</Link></li>
+        <li><Link to="/projects" onClick={handleCloseNav}>Projects</Link></li>
+        <li><Link to="/career" onClick={handleCloseNav} >Career</Link></li>
+        <li><Link to="/contacts" onClick={handleCloseNav}>Contacts</Link></li>
  
 
         </ul>
-        <label htmlFor="menu-btn" className="btn menu-btn"><i className="bi bi-list" />=</label>
+        <label htmlFor="menu-btn" className="btn menu-btn"><i className="bi bi-list" /><FiMenu /></label>
    
         
    

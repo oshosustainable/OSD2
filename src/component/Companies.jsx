654273ce6b8c33d5288b742750d65524a0b4aed6
satishmahat <mahatsatish@gmail.com>
2025-04-01 22:25:13 +0545

    Refactor: Replace SCSS with pure CSS

--- src/component/Companies.jsx
+++ src/component/Companies.jsx
@@ -1,69 +1,31 @@
-import React from 'react'
-import './companies.scss'
+import React from "react";
+import "./companies.css";
 
-import client1 from './images/clients/adbl.png'
-import client2 from './images/clients/court.png'
-import client3 from './images/clients/buddha.png'
-import client4 from './images/clients/mbbl.png'
-import client5 from './images/clients/daraz.png'
-import client6 from './images/clients/socialdev.png'
-import client7 from './images/clients/swiss.png'
+import client1 from "./images/clients/adbl.png";
+import client2 from "./images/clients/court.png";
+import client3 from "./images/clients/buddha.png";
+import client4 from "./images/clients/mbbl.png";
+import client5 from "./images/clients/daraz.png";
+import client6 from "./images/clients/socialdev.png";
+import client7 from "./images/clients/swiss.png";
 
-
+const clients = [client1, client2, client3, client4, client5, client6, client7];
 
 const Companies = () => {
   return (
-    <div className='companies-main'>
-        <h2>Our Clients</h2>
-    <div className="slider">
-    <div className="slide-track">
-    <div className="slide">
-        <img src={client1} height={120} width={300}  />
+    <div className="companies-section">
+      <h2>Our Clients</h2>
+      <div className="companies-slider">
+        <div className="companies-track">
+          {clients.concat(clients).map((client, index) => (
+            <div className="companies-item" key={index}>
+              <img src={client} height={120} width={300} alt="Client Logo" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-    <div className="slide">
-        <img src={client2} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client3} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client4} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client5} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client6} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client7} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client1} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client2} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client3} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client4} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client5} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client6} height={120} width={300}  />
-    </div>
-    <div className="slide">
-        <img src={client7} height={120} width={300}  />
-    </div>
-    </div>
-    </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Companies
+export default Companies;
